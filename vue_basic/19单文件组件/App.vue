298b26e733693939cc735e20794cd8c5d2c17773
gx <2416457672@qq.com>
2022-07-29 17:26:48 +0800

    7.29

---
+++ vue_basic/19单文件组件/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <School></School>
+	    <Student></Student>
+    </div> 
+</template>
+
+<script>
+    import School from './School'
+    import Student from './Student' 
+    export default {
+        name:'App',
+        compoments:{
+            School,
+            Student
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ vue_test/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Student name="李四" sex="女" :age="18"/>
+    </div>
+</template>
+
+<script>
+    import Student from './components/Sutdent.vue'
+
+    export default {
+        name:'App',
+        components:{
+            Student
+        },
+       
+    }
+</script>
+
+<style>
+
+</style>
