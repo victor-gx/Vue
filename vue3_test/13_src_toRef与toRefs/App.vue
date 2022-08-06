d0079aa284015f0556e8c15450839a53e597d803
gx <2416457672@qq.com>
2022-08-06 12:06:19 +0800

    8.6

---
+++ vue3_test/13_src_toRef与toRefs/App.vue
@@ -0,0 +1,17 @@
+<template>
+  	<Demo/>
+</template>
+
+<script>
+	import {ref} from 'vue'
+	import Demo from './components/Demo'
+	export default {
+		name: 'App',
+		components:{Demo},
+		setup() {
+			let isShowDemo = ref(true)
+			return {isShowDemo}
+		}
+	}
+</script>
+
