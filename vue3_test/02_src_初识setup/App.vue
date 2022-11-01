9b694c44394b8ea96918af7d66a89544089d8a73
gx <2416457672@qq.com>
2022-11-01 15:44:56 +0800

    11.1

--- vue3_test/02_src_初识setup/App.vue
+++ vue3_test/02_src_初识setup/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<h1>我是App组件</h1>
+	<h1>一个人的信息</h1>
 	<h2>姓名：{{name}}</h2>
 	<h2>年龄：{{age}}</h2>
 	<h2>性别：{{sex}}</h2>
@@ -18,19 +18,18 @@
 </template>
 
 <script>
-  // import {h} from 'vue'
-  export default {
-    name: 'App',
-
-    data() {
-      return {
-        sex:'男',
-        a:100
-      }
-    },
-    methods: {
+	// import {h} from 'vue'
+	export default {
+		name: 'App',
+		data() {
+			return {
+				sex:'男',
+				a:100
+			}
+		},
+		methods: {
 			sayWelcome(){
-				alert('欢迎学习Vue')
+				alert('欢迎来到尚硅谷学习')
 			},
 			test1(){
 				console.log(this.sex)
@@ -39,17 +38,18 @@
 				console.log(this.sayHello)
 			}
 		},
-    
-    setup(){
-      //数据
+		//此处只是测试一下setup，暂时不考虑响应式的问题。
+		async setup(){
+			//数据
 			let name = '张三'
 			let age = 18
-      let a = 200
+			let a = 200
 
-      function sayHello() {
-        alert(`我叫${name}，我${age}岁了，你好啊！`)
-      }
-      function test2(){
+			//方法
+			function sayHello(){
+				alert(`我叫${name}，我${age}岁了，你好啊！`)
+			}
+			function test2(){
 				console.log(name)
 				console.log(age)
 				console.log(sayHello)
@@ -57,18 +57,18 @@
 				console.log(this.sayWelcome)
 			}
 
-      //返回一个对象（常用）
+			//返回一个对象（常用）
 			return {
 				name,
 				age,
 				sayHello,
-        test2,
-        a
+				test2,
+				a
 			}
 
-      //返回一个函数（渲染函数）
-			// return ()=> h('h1','gx')
-    }
-  }
+			//返回一个函数（渲染函数）
+			// return ()=> h('h1','尚硅谷')
+		}
+	}
 </script>
 
